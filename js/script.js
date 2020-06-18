e98aebda27f14f100257e34d18a8a41b07c0dfd3
wwwcoderwww <wwwcoderwww@gmail.com>
2020-06-18 23:28:27 +0300

    Done slider

--- js/script.js
+++ js/script.js
@@ -6,17 +6,26 @@ var $slider = $('.slider');
 if ($slider.length) {
   var currentSlide;
   var slidesCount;
+  var sliderPhotos;
   var sliderCounter = document.createElement('div');
+  var sliderPhotos = document.createElement('div');
+  var sliderMap = document.createElement('div');
   sliderCounter.classList.add('slider__counter');
+  sliderPhotos.classList.add('slider__photos');
+  sliderMap.classList.add('slider__map');
   
   var updateSliderCounter = function(slick, currentIndex) {
     currentSlide = slick.slickCurrentSlide() + 1;
     slidesCount = slick.slideCount;
     $(sliderCounter).text(currentSlide + '/' +slidesCount)
+    $(sliderPhotos).text('Photos')
+    $(sliderMap).text('Map')
   };
 
   $slider.on('init', function(event, slick) {
     $slider.append(sliderCounter);
+    $slider.append(sliderPhotos);
+    $slider.append(sliderMap);
     updateSliderCounter(slick);
   });
 
