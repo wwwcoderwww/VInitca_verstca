e68bf80b7eadb90c55f5f631b35f73db1655a742
wwwcoderwww <wwwcoderwww@gmail.com>
2020-06-19 13:46:36 +0300

    Done auction input

--- js/script.js
+++ js/script.js
@@ -35,3 +35,20 @@ if ($slider.length) {
 
   $slider.slick();
 }
+
+
+$(document).ready(function(){
+  $('.count').prop('disabled', true);
+  $(document).on('click','.plus',function(){
+$('.count').val(parseInt($('.count').val()) + 1 );
+if ($('.count').val() == 0) {
+  $('.count').val(1);
+ }
+  });
+     $(document).on('click','.minus',function(){
+   $('.count').val(parseInt($('.count').val()) - 1 );
+    if ($('.count').val() == 0) {
+  $('.count').val(1);
+ }
+      });
+});
